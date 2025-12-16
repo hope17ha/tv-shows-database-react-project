101f234d5992ea869acf979d2d8ccdbb773d9bbf
hope17ha <n.anastasova91@gmail.com>
2025-12-16 10:33:08 +0200

    added: helper validator for correct data when login and register

--- client/src/api/authApi.js
+++ client/src/api/authApi.js
@@ -2,17 +2,28 @@ import { useEffect, useState } from "react";
 import requester from "../utils/requester";
 import { useUserContext } from "../contexts/UserContext";
 import useAuth from "../hooks/useAuth";
+import { isValidMail } from "../utils/validators";
 
 const baseUrl = "http://localhost:3030/users";
 
 export const useLogin = () => {
 
-    const login = (email, password) =>
+    const login = (email, password) => {
+
+        if (!isValidMail(email)){
+            throw new Error ('Please enter a valid email address!')
+        }
+
+        if (!password) {
+            throw new Error('Password is required');
+        }
+
         requester.post(
             `${baseUrl}/login`,
             { email, password },
-        );
-
+            );
+            
+        }
     return {
         login,
     }
@@ -20,13 +31,25 @@ export const useLogin = () => {
 
 export const useRegister = () => {
 
-    const register = (email, username, password) => 
+    const register = (email, username, password) => {
+
+        if (!isValidMail(email)){
+            throw new Error ('Please enter a valid email address!')
+        }
+        if (username.length < 3) {
+            throw new Error("Username must be at least 3 characters");
+        }
+
+        if (password.length < 6) {
+            throw new Error("Password must be at least 6 characters");
+        }
+
         requester.post(
             `${baseUrl}/register`,
          {email, username, password}
          );
     
-
+        }
     return {
         register,
     }
