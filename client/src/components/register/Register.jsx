4b3832a5b8526aaa73579d6b2fcb43d4f8d51947
hope17ha <n.anastasova91@gmail.com>
2025-12-16 10:40:02 +0200

    fixed: reset of fields when entering incorrect data in register page

--- client/src/components/register/Register.jsx
+++ client/src/components/register/Register.jsx
@@ -10,8 +10,11 @@ export default function Register() {
     const { register } = useRegister()
     const { userLoginHandler } = useUserContext();
 
-    const registerHandler = async (formData) => {
-         const { email, username, password, rePassword } = Object.fromEntries(formData);
+    const registerHandler = async (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.target);
+        const { email, username, password, rePassword } = Object.fromEntries(formData);
 
          if (password !== rePassword){
             
@@ -40,7 +43,7 @@ export default function Register() {
                     <div className="info-register">
                         <h2>"I hope you're pleased with yourselves. We could all have been killed — or worse, expelled. Now if you don't mind, I'm going to bed."</h2>
                     </div>
-                    <form action={registerHandler}
+                    <form onSubmit={registerHandler}
                     className="registerForm">
                         <h2>Register</h2>
                         <ul className="noBullet">
@@ -50,18 +53,18 @@ export default function Register() {
                             </li>
                             <li>
                                 <label htmlFor="username">Username:</label>
-                                <input type="text" className="inputFields" id="username" name="username" />
+                                <input type="text" minLength={3} className="inputFields" id="username" name="username" />
                             </li>
                             <li>
                                 <label htmlFor="password">Password:</label>
-                                <input type="password" className="inputFields" id="password" name="password"/>
+                                <input type="password" minLength={6} className="inputFields" id="password" name="password"/>
                             </li>
                             <li>
                                 <label htmlFor="repeat-password">Repeat-Password:</label>
                                 <input type="password" className="inputFields" id="repeat-password" name="rePassword"/>
                             </li>
                             <li id="center-btn">
-                                <button id="join-btn">Register</button>
+                                <button id="join-btn" type="submit">Register</button>
                             </li>
                         </ul>
                     </form>
